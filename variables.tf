13d72c5a650b82a9a9f2c2968f6a1ee5d4545bbd
saurabhkothari <saurabhkothari@rocketmail.com>
2022-05-05 19:30:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,8 +6,27 @@ variable "intersight_endpoint" {}
 
 variable "org_name" {}
 
-variable "server_profile_name" {}
+variable "server_list" {
+  type        = list(map(string))
+  description = "Servers (identified by name, object_type, and target_platform) to assign to configured server profiles"
+  default = [
+    {
+      name                   = "BGL101-R1-1",
+      target_platform        = "Standalone",
+      server_profile_action  = "unassign"
+    },
+    {
+      name                   = "BGL101-R1-2",
+      target_platform        = "Standalone",
+      server_profile_action  = "unassign"
+    },
+    {
+      name                   = "BGL101-R1-FI101-1",
+      target_platform        = "FIAttached",
+      server_profile_action  = "unassign"
+    }
+  ]
+}
 
-variable "server_profile_action" {}
 
-variable "server_profile_target_platform" {}
+
