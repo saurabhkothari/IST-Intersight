5ff902dc7b008181b056a50bd0cb80208f1898d0
saurabhkothari <saurabhkothari@rocketmail.com>
2022-05-05 19:29:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "intersight_api_key" {}
+
+variable "intersight_secret_key" {}
+
+variable "intersight_endpoint" {}
+
+variable "org_name" {}
+
+variable "server_profile_name" {}
+
+variable "server_profile_action" {}
+
+variable "server_profile_target_platform" {}
